ec87dada597dcc93b77c0055d91d43f4c55041fb
mouhamad5210 <60216949+mouhamad5210@users.noreply.github.com>
2021-11-02 23:27:26 +0100

    Update SBinTre.java

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -323,22 +323,6 @@ public class SBinTre<T> {
         return ttre;
     }
 
-
-
-    public static void main(String[] args) {
-        
-        SBinTre<Integer> tre =
-                new SBinTre<>(Comparator.naturalOrder());
-        int[] a = {10, 14, 6, 8, 1, 12, 7, 3, 11, 9, 13, 5, 2, 4};
-        for (int verdi : a) { tre.leggInn(verdi); }
-        //Gjør om treet til et array
-        ArrayList<Integer> data = tre.serialize();
-        //Lag nytt tre fra arrayet over
-        SBinTre<Integer> tre2 = SBinTre.deserialize(data, Comparator.naturalOrder());
-        //Utskriften av tre og tre2 skal være identiske
-        System.out.println(tre.toStringPostOrder());
-        System.out.println(tre2.toStringPostOrder());
-    }
         
 
 } // ObligSBinTre
